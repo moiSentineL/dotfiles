fc2091205d4a8de8a1065f4c261cd61d09440716
moiSentineL <nibir.patowary@gmail.com>
2024-12-10 23:52:27 +0530

    waybar: delete faulty scroll.sh

--- waybar/scripts/scroll-player.sh
+++
@@ -1,7 +0,0 @@
-CURRENT_SONG=$(python3 $HOME/.config/waybar/scripts/mediaplayer.py --player spotify)
-
-zscroll -p " | " --delay 0.2 \
-    --length 40 \
-    --update-interval 1 \
-    --update-check true $CURRENT_SONG &
-wait
