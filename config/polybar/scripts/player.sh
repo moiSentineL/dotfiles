ed049d2cdc204740efccf45ad0f1d8e9e23b1535
moiSentineL <nibir.patowary@gmail.com>
2025-05-01 14:16:42 +0530

    polybar: improve media control module

--- config/polybar/scripts/player.sh
+++ config/polybar/scripts/player.sh
@@ -1,12 +1,18 @@
-#!/bin/bash
+#!/bin/zsh
 
-ARTIST="$(playerctl metadata xesam:artist)"
-TITLE="$(playerctl metadata xesam:title)"
+PLAYERS="spotify,cmus"
+COMMAND="playerctl --player=$PLAYERS status"
 
+write() {
+    if [[ $(playerctl --player=$PLAYERS status) = "Playing" ]]; then
+        echo "$(playerctl metadata --format "{{ artist }} - {{ title }}")"
+    elif [[ $(playerctl --player=$PLAYERS status) = "Paused" ]]; then
+        echo "  $(playerctl metadata --format "{{ artist }} - {{ title }}")"
+    fi
+}
 
-if [[ $(playerctl status) = "Playing" ]]; then
-    echo "$ARTIST - $TITLE"
-elif [[ $(playerctl status) = "Paused" ]]; then
-    echo "  $ARTIST - $TITLE"
+if [[ $(playerctl -l 2>&1) != *"No players found"* ]]; then
+    write
+else
+    echo ""
 fi
-
